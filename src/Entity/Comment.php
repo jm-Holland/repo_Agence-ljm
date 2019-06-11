0cd02a534b300ff6bcc68e3683b6fb833d33ba93
illaweb35 <pierre.durand@domaine.fr>
2019-06-11 18:54:37 +0200

    Entity Article & Comment

---
+++ src/Entity/Comment.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CommentRepository")
+ * @ORM\HasLifecycleCallbacks()
+ */
+class Comment
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     * @Assert\Length(
+     *      min = 2,
+     *      max = 50,
+     *      minMessage = "Votre pseudo doit avoie au minimum {{ limit }} caractères",
+     *      maxMessage = "Votre pseudo ne doit pas excèder plus de  {{ limit }} caractères."
+     * )
+     */
+    private $author;
+
+    /**
+     * @ORM\Column(type="text")
+     * @Assert\NotBlank
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Article", inversedBy="comments")
+     */
+    private $article;
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function setCreatedAtValue()
+    {
+        $this->createdAt = new \DateTime();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getArticle(): ?Article
+    {
+        return $this->article;
+    }
+
+    public function setArticle(?Article $article): self
+    {
+        $this->article = $article;
+
+        return $this;
+    }
+}
---
+++ src/Migrations/Version20190611163933.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190611163933 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('CREATE TABLE comment (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, article_id INTEGER DEFAULT NULL, author VARCHAR(255) NOT NULL, content CLOB NOT NULL, created_at DATETIME NOT NULL)');
+        $this->addSql('CREATE INDEX IDX_9474526C7294869C ON comment (article_id)');
+        $this->addSql('DROP INDEX IDX_23A0E66F675F31B');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__article AS SELECT id, author_id, title, image, content, created_date FROM article');
+        $this->addSql('DROP TABLE article');
+        $this->addSql('CREATE TABLE article (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, author_id INTEGER NOT NULL, title VARCHAR(255) NOT NULL COLLATE BINARY, image VARCHAR(255) DEFAULT NULL COLLATE BINARY, content CLOB NOT NULL COLLATE BINARY, created_date DATETIME NOT NULL, CONSTRAINT FK_23A0E66F675F31B FOREIGN KEY (author_id) REFERENCES user (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('INSERT INTO article (id, author_id, title, image, content, created_date) SELECT id, author_id, title, image, content, created_date FROM __temp__article');
+        $this->addSql('DROP TABLE __temp__article');
+        $this->addSql('CREATE INDEX IDX_23A0E66F675F31B ON article (author_id)');
+        $this->addSql('DROP INDEX IDX_BF9473341645DEA9');
+        $this->addSql('DROP INDEX IDX_BF947334BAD26311');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__tag_reference AS SELECT tag_id, reference_id FROM tag_reference');
+        $this->addSql('DROP TABLE tag_reference');
+        $this->addSql('CREATE TABLE tag_reference (tag_id INTEGER NOT NULL, reference_id INTEGER NOT NULL, PRIMARY KEY(tag_id, reference_id), CONSTRAINT FK_BF947334BAD26311 FOREIGN KEY (tag_id) REFERENCES tag (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_BF9473341645DEA9 FOREIGN KEY (reference_id) REFERENCES reference (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('INSERT INTO tag_reference (tag_id, reference_id) SELECT tag_id, reference_id FROM __temp__tag_reference');
+        $this->addSql('DROP TABLE __temp__tag_reference');
+        $this->addSql('CREATE INDEX IDX_BF9473341645DEA9 ON tag_reference (reference_id)');
+        $this->addSql('CREATE INDEX IDX_BF947334BAD26311 ON tag_reference (tag_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('DROP TABLE comment');
+        $this->addSql('DROP INDEX IDX_23A0E66F675F31B');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__article AS SELECT id, author_id, title, image, content, created_date FROM article');
+        $this->addSql('DROP TABLE article');
+        $this->addSql('CREATE TABLE article (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, author_id INTEGER NOT NULL, title VARCHAR(255) NOT NULL, image VARCHAR(255) DEFAULT NULL, content CLOB NOT NULL, created_date DATETIME NOT NULL)');
+        $this->addSql('INSERT INTO article (id, author_id, title, image, content, created_date) SELECT id, author_id, title, image, content, created_date FROM __temp__article');
+        $this->addSql('DROP TABLE __temp__article');
+        $this->addSql('CREATE INDEX IDX_23A0E66F675F31B ON article (author_id)');
+        $this->addSql('DROP INDEX IDX_BF947334BAD26311');
+        $this->addSql('DROP INDEX IDX_BF9473341645DEA9');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__tag_reference AS SELECT tag_id, reference_id FROM tag_reference');
+        $this->addSql('DROP TABLE tag_reference');
+        $this->addSql('CREATE TABLE tag_reference (tag_id INTEGER NOT NULL, reference_id INTEGER NOT NULL, PRIMARY KEY(tag_id, reference_id))');
+        $this->addSql('INSERT INTO tag_reference (tag_id, reference_id) SELECT tag_id, reference_id FROM __temp__tag_reference');
+        $this->addSql('DROP TABLE __temp__tag_reference');
+        $this->addSql('CREATE INDEX IDX_BF947334BAD26311 ON tag_reference (tag_id)');
+        $this->addSql('CREATE INDEX IDX_BF9473341645DEA9 ON tag_reference (reference_id)');
+    }
+}

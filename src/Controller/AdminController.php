6781361d4c9604e1a1e296f7ca1c05bd68c5a824
illaweb35 <pierre.durand@domaine.fr>
2019-06-13 19:34:54 +0200

    Admin user

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,22 +2,29 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use App\Repository\ArticleRepository;
 use App\Repository\CommentRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * @Route("/admin")
+ * @IsGranted("ROLE_USER")
  */
 class AdminController extends AbstractController
 {
     /**
      * @Route("/DashBoard", name="admin_index")
      */
-    public function index()
+    public function index(ArticleRepository $articles, CommentRepository $comments, UserRepository $users)
     {
-        return $this->render('admin/index.html.twig');
+        return $this->render('admin/index.html.twig', [
+            'articles' => $articles->findAll(),
+            'comments' => $comments->findAll(),
+            'users' => $users->findAll()
+        ]);
     }
 
     /**

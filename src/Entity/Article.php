e2d3097b91f4584583acdc4f841c0293d6006048
illaweb35 <pierre.durand@domaine.fr>
2019-06-15 15:16:59 +0200

    import class manquante

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -2,11 +2,12 @@
 
 namespace App\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
-use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Validator\Constraints as Assert;
+use App\Entity\Comment;
 use App\Service\UploaderHelper;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")

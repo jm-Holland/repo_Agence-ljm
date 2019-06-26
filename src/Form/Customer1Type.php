c5eed30562cbc246d9b74cd0cf8fea6d48b6ddc3
illaweb35 <illaweb35@gmail.com>
2019-06-26 17:54:50 +0200

    Modifications dashboard pour customer

---
+++ src/Form/Customer1Type.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Customer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class Customer1Type extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName')
+            ->add('lastName')
+            ->add('email')
+            ->add('company')
+            ->add('subject')
+            ->add('address')
+            ->add('postalCode')
+            ->add('city')
+            ->add('country')
+            ->add('content')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Customer::class,
+        ]);
+    }
+}

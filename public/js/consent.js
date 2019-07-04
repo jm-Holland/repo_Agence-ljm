4a2f410ae86e81dfcfc042a5074bc5456dfc4e79
illaweb35 <illaweb35@gmail.com>
2019-07-04 11:05:35 +0200

    Modification cookies consent

--- public/js/consent.js
+++ public/js/consent.js
@@ -4,8 +4,8 @@
         request.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
                 var data = JSON.parse(request.responseText);
-                var eu_country_codes = ['AL', 'AD', 'AM', 'AT', 'BY', 'BE', 'BA', 'BG', 'CH', 'CY', 'CZ', 'DE', 'DK', 'EE', 'ES', 'FO', 'FI', 'FR', 'GB', 'GE', 'GI', 'GR', 'HU', 'HR', 'IE', 'IS', 'IT', 'LT', 'LU', 'LV', 'MC', 'MK', 'MT', 'NO', 'NL', 'PO', 'PT', 'RO', 'RU', 'SE', 'SI', 'SK', 'SM', 'TR', 'UA', 'VA'];
-                if (eu_country_codes.indexOf(data.countryCode) != -1) {
+                var country_code2 = ['AL', 'AD', 'AM', 'AT', 'BY', 'BE', 'BA', 'BG', 'CH', 'CY', 'CZ', 'DE', 'DK', 'EE', 'ES', 'FO', 'FI', 'FR', 'GB', 'GE', 'GI', 'GR', 'HU', 'HR', 'IE', 'IS', 'IT', 'LT', 'LU', 'LV', 'MC', 'MK', 'MT', 'NO', 'NL', 'PO', 'PT', 'RO', 'RU', 'SE', 'SI', 'SK', 'SM', 'TR', 'UA', 'VA'];
+                if (country_code2.indexOf(data.countryCode2) != -1) {
                     document.body.innerHTML += '\
 					<div class="cookieconsent" style="position:fixed;padding:20px;left:0;bottom:0;background-color:#000;color:#FFF;text-align:center;width:100%;z-index:99999;">\
                     Ce site utilise des cookies. En poursuivant votre navigation sur ce site, vous acceptez leur utilisation. \
@@ -20,7 +20,7 @@
                 }
             }
         };
-        request.open('GET', 'http://ip-api.com/json', true);
+        request.open('GET', 'https://api.ipgeolocation.io/ipgeo?apiKey=b208ef369c1a4773bcd49740f4f7ea6a', true);
         request.send();
     }
 })();

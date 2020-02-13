d1ed0e91f6187debcef34a62413beee3a332ae32
jmh35310 <jmholland@illaweb.fr>
2020-02-13 11:30:28 +0100

    update symfony 4.4

--- src/Repository/ReferenceRepository.php
+++ src/Repository/ReferenceRepository.php
@@ -4,7 +4,8 @@ namespace App\Repository;
 
 use App\Entity\Reference;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
+
 
 /**
  * @method Reference|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +15,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class ReferenceRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Reference::class);
     }
